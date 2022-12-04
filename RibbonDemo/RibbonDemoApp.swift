5cb462620a868a0b111a8abc7323c76d02db8432
Julien Ducret <brocoo@gmail.com>
2022-12-04 17:36:20 +0000

    Initial Commit

---
+++ RibbonDemo/RibbonDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RibbonDemoApp.swift
+//  RibbonDemo
+//
+//  Created by Julien Ducret on 04/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct RibbonDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
